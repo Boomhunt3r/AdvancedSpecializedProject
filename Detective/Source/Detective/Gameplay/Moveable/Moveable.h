fed1e103b38b1b4b33feb61fe35a3e0622f0e5a1
Jannek <jannekvieth@outlook.de>
2020-05-04 18:27:41 +0200

    Moveable now works properly

--- Detective/Source/Detective/Gameplay/Moveable/Moveable.h
+++ Detective/Source/Detective/Gameplay/Moveable/Moveable.h
@@ -17,10 +17,20 @@ class DETECTIVE_API AMoveable : public AActor
 	GENERATED_BODY()
 	
 public:	
+#pragma region constructor
 	// Sets default values for this actor's properties
 	AMoveable();
+#pragma endregion
 
+
+#pragma region UPROPERTY
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadWrite, Category = "Moveable")
 	UStaticMeshComponent* Mesh = nullptr;
+#pragma endregion
 
+
+#pragma region public function
 	bool Move(FVector _movement);
+#pragma endregion
+
 };
