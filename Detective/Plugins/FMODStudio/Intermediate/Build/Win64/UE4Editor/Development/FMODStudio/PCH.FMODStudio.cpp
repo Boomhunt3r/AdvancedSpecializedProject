a9983f0e1dc54e44f4f8365306cf8cc50aa0b5f5
Cedric <cedric.lesche@gmail.com>
2020-06-12 15:04:32 +0200

    some changes to character, level, and echos

--- Detective/Plugins/FMODStudio/Intermediate/Build/Win64/UE4Editor/Development/FMODStudio/PCH.FMODStudio.cpp
+++ Detective/Plugins/FMODStudio/Intermediate/Build/Win64/UE4Editor/Development/FMODStudio/PCH.FMODStudio.cpp
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-#include "G:/Projects/BachelorGruppenArbeit/AdvancedSpecializedProject/Detective/Plugins/FMODStudio/Intermediate/Build/Win64/UE4Editor/Development/FMODStudio/PCH.FMODStudio.h"
-=======
 #include "D:/Uni/A_Spiele/Detective/Git/AdvancedSpecializedProject/Detective/Plugins/FMODStudio/Intermediate/Build/Win64/UE4Editor/Development/FMODStudio/PCH.FMODStudio.h"
->>>>>>> develop
