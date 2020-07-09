9afd4712526b01172d2f83d8d8a9680b03da25d3
Georg <mail@kleingeo.com>
2020-07-09 19:43:33 +0200

    Fixed stuff, adjusted Lightmap density, added pipes LODs

--- Detective/Plugins/FMODStudio/Intermediate/Build/Win64/UE4Editor/Development/FMODStudioEditor/PCH.FMODStudioEditor.h
+++ Detective/Plugins/FMODStudio/Intermediate/Build/Win64/UE4Editor/Development/FMODStudioEditor/PCH.FMODStudioEditor.h
@@ -1,4 +1,4 @@
-// PCH for C:\Users\Falk\Documents\GitKraken\AdvancedSpecializedProject\Detective\Plugins\FMODStudio\Source\FMODStudioEditor\Private\FMODStudioEditorPrivatePCH.h
+// PCH for D:\AdvancedSpecializedProject\Detective\Plugins\FMODStudio\Source\FMODStudioEditor\Private\FMODStudioEditorPrivatePCH.h
 #define IS_PROGRAM 0
 #define UE_EDITOR 1
 #define ENABLE_PGO_PROFILE 0
@@ -390,4 +390,4 @@
 #define FMODSTUDIO_API DLLIMPORT
 #define MEDIA_VTABLE DLLIMPORT_VTABLE
 #define MEDIA_API DLLIMPORT
-#include "C:/Users/Falk/Documents/GitKraken/AdvancedSpecializedProject/Detective/Plugins/FMODStudio/Source/FMODStudioEditor/Private/FMODStudioEditorPrivatePCH.h"
+#include "D:/AdvancedSpecializedProject/Detective/Plugins/FMODStudio/Source/FMODStudioEditor/Private/FMODStudioEditorPrivatePCH.h"
