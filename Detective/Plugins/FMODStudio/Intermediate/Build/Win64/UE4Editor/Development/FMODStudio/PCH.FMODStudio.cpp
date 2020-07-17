46cf93c79cd6b98c48f248d869f52c2ba15f1035
Falk <blackscreen899@gmail.com>
2020-07-17 16:18:35 +0200

    Tutorial is connected (with some bugs)

--- Detective/Plugins/FMODStudio/Intermediate/Build/Win64/UE4Editor/Development/FMODStudio/PCH.FMODStudio.cpp
+++ Detective/Plugins/FMODStudio/Intermediate/Build/Win64/UE4Editor/Development/FMODStudio/PCH.FMODStudio.cpp
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 #include "C:/Users/Falk/Documents/GitKraken/AdvancedSpecializedProject/Detective/Plugins/FMODStudio/Intermediate/Build/Win64/UE4Editor/Development/FMODStudio/PCH.FMODStudio.h"
-=======
-#include "F:/BachelorTeam/AdvancedSpecializedProject/AdvancedSpecializedProject/Detective/Plugins/FMODStudio/Intermediate/Build/Win64/UE4Editor/Development/FMODStudio/PCH.FMODStudio.h"
->>>>>>> develop
